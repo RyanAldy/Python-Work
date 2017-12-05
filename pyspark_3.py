98276dadfe44205b2d832567a756ed32296da1bf
Ryan Alderson <ryanalderson91@gmail.com>
2017-12-05 14:05:44 +0000

    Added new file

---
+++ pyspark_3.py
@@ -0,0 +1,17 @@
+from pyspark import SparkContext, SparkConf
+
+def records(rec):
+    Data = rec.split(",")
+    amount = int(Data[6]) * int(Data[7])
+    return (Data[1], amount)
+
+
+c = SparkConf()
+sc = SparkContext(conf = c)
+
+RDD = sc.textfile("sales.csv")
+First = RDD.first()
+
+RDD2 = RDD.filter(lambda x: x <> First)
+RDD3 = RDD2.map(records)
+Data = RDD4.collect()
